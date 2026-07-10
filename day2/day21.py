f63e8296ac84713e96665a80190e45bc4556e5a5
Hav <99307586+341t5u@users.noreply.github.com>
2026-07-10 20:01:48 +0300

    Fix errors and update code

--- day2/day21.py
+++ day2/day21.py
@@ -6,7 +6,7 @@ friends=[
 def split_bill(price, rate=0.10):
     num_of_friends=3     
     tip =price * rate
-    split=price / num_of_friends
-    return tip+split
+    split=price + tip
+    return split/num_of_friends
 for name in friends:
     print(f"{name}: You have to share {split_bill(3600)} ETB ")
