124b93a00a2b870f45e4ce6d1351cc462f77be34
Hav <99307586+341t5u@users.noreply.github.com>
2026-07-16 21:33:21 +0300

    Day 5 project

--- day4/account.py
+++ day4/account.py
@@ -10,8 +10,29 @@ class Account:
         if amount <= 0:
             raise ValueError("Insufficient balance")
         self.__balance += amount
+class SavingAccount(Account):
+    def __init__(self, owner, number, balance=0,rate=0.05):
+        super().__init__(owner, number, balance)
+        self.rate= rate
+    def add_interest(self):
+        self.deposit(self.balance*self.rate)
+class CurrentAccount(Account):
+    def __init__(self, owner, number, balance=0,od=1000):
+        super().__init__(owner, number, balance)
+        self.overdraft=od
+    def withdraw(self,amount):
+        if amount>self.balance + self.overdraft:
+            raise ValueError("Over limit")
+        self._Account__balance -= amount 
 acc = Account(
 "Almaz", "CBE-1001", 1500)
 acc.deposit(500)
 print(f"Your balance is {acc.balance}")
-# 2000
+s = SavingAccount(
+"Almaz", "CBE-1001", 1500)
+s.deposit(500) # inherited
+s.add_interest() # new
+print(s.balance)
+c=CurrentAccount("Almaz", "CBE-1001", 1500)
+c.withdraw(700)
+print(c.balance)
